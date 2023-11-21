afaa2d01e842e465b1c8cf1af16681658537c332
Andres Torres <santiago.ojedatorres@gmail.com>
2023-11-21 18:55:52 -0500

    Changes

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 from fastapi import FastAPI
+from fastapi.openapi.models import OAuthFlows as OAuthFlowsModel
+from fastapi.openapi.models import OAuthFlowAuthorizationCode
 
 app = FastAPI()
 
-@app.get("/")
-async def root():
-    return {"greeting": "Hello, World!", "message": "Welcome to FastAPI!"}
+@app.get("/items/{item_id}")
+async def read_item(item_id: int, query_param: str = None):
+    return {"item_id": item_id, "query_param": query_param}
