b06662771a07e67a387d5470c95bb6768d9ca988
Andres Torres <santiago.ojedatorres@gmail.com>
2023-11-21 22:12:41 -0500

    Changed the way

--- main.py
+++ main.py
@@ -1,30 +1,37 @@
-from fastapi import FastAPI, UploadFile, File
-from tensorflow.keras.models import load_model
+from fastapi import FastAPI, File, UploadFile
 from PIL import Image
 import numpy as np
 import io
+import gdown
+import numpy as np
+import tensorflow as tf
+import keras
+
+# URL de Google Drive
+url = 'https://drive.google.com/uc?id=1wMb03-UkWY2PmWkvZKUxXZppuINfOFza'
+
+# Descarga el archivo
+gdown.download(url, 'my_trained_model.h5', quiet=False)
+
+# Carga el modelo
+model = keras.models.load_model('my_trained_model.h5')
 
 app = FastAPI()
 
-MODEL_PATH = "model.h5"
-model = load_model(MODEL_PATH)
-
 @app.post("/predict/")
-async def make_prediction(file: UploadFile = File(...)):
-    # Convertir la imagen subida en un formato que el modelo pueda procesar
-    image = await file.read()
-    image = Image.open(io.BytesIO(image))
-    processed_image = preprocess_image(image)
+async def create_upload_file(file: UploadFile = File(...)):
+    # Lee la imagen
+    image_data = await file.read()
+    image = Image.open(io.BytesIO(image_data))
 
-    # Hacer una predicción con el modelo
-    prediction = model.predict(processed_image)
-    return {"prediction": prediction}
-
-def preprocess_image(image):
-    # Aquí debes ajustar la imagen a las necesidades de tu modelo.
-    # Por ejemplo, cambiar el tamaño, normalizar, etc.
-    image = image.resize((224, 224))
+    # Preprocesa la imagen para tu modelo
+    # (este paso depende de cómo entrenaste tu modelo)
+    image = image.resize((128, 128))  # ejemplo de redimensionamiento
     image_array = np.array(image)
-    image_array = image_array / 255.0  # normalizar si es necesario
-    image_array = np.expand_dims(image_array, axis=0)  # expandir dimensiones si el modelo lo requiere
-    return image_array
+
+    # Predice con tu modelo
+    prediction = model.predict(np.array([image_array]))
+
+    # Procesa y devuelve la predicción
+    # (este paso depende de tu modelo)
+    return {"prediction": prediction.tolist()}
