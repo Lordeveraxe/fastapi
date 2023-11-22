46192dfc71bad500900b1ffb9475357b9f71d538
Andres Torres <santiago.ojedatorres@gmail.com>
2023-11-21 21:47:58 -0500

    Added model

--- main.py
+++ main.py
@@ -1,9 +1,30 @@
-from fastapi import FastAPI
-from fastapi.openapi.models import OAuthFlows as OAuthFlowsModel
-from fastapi.openapi.models import OAuthFlowAuthorizationCode
+from fastapi import FastAPI, UploadFile, File
+from tensorflow.keras.models import load_model
+from PIL import Image
+import numpy as np
+import io
 
 app = FastAPI()
 
-@app.get("/items/{item_id}")
-async def read_item(item_id: int, query_param: str = None):
-    return {"item_id": item_id, "query_param": query_param}
+MODEL_PATH = "model.h5"
+model = load_model(MODEL_PATH)
+
+@app.post("/predict/")
+async def make_prediction(file: UploadFile = File(...)):
+    # Convertir la imagen subida en un formato que el modelo pueda procesar
+    image = await file.read()
+    image = Image.open(io.BytesIO(image))
+    processed_image = preprocess_image(image)
+
+    # Hacer una predicción con el modelo
+    prediction = model.predict(processed_image)
+    return {"prediction": prediction}
+
+def preprocess_image(image):
+    # Aquí debes ajustar la imagen a las necesidades de tu modelo.
+    # Por ejemplo, cambiar el tamaño, normalizar, etc.
+    image = image.resize((224, 224))
+    image_array = np.array(image)
+    image_array = image_array / 255.0  # normalizar si es necesario
+    image_array = np.expand_dims(image_array, axis=0)  # expandir dimensiones si el modelo lo requiere
+    return image_array
